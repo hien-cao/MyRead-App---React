307a81edc2f4a7b50922e62cdc4b28595ddbbc13
Hien <caoduonghien@gmail.com>
2018-08-25 09:50:05 +0300

    Structure app.js with two component ListBook and BookSearch

---
+++ src/components/BookSearch.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class BookSearch extends Component {
+  render() {
+    return (
+      <div>Book Search</div>
+    )
+  }
+}
+
+export default BookSearch 
---
+++ src/components/ListBook.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import BookShelf from './component/BookShelf'
+
+
+class ListBook extends Component {
+  render() {
+    return (
+      <div className='list-books'>
+        <div className='list-books-title'>
+          <h1>MyReads</h1>
+        </div>
+        <div className='list-books-content'>
+          <BookShelf/>
+        </div>
+        <div className='open-search'>
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ListBook
+
